175c02f2a3ebefc96e774ca409e8e65be9690b64
Giorgio Pieretti <giorgio.pieretti@openprojects.it>
2014-01-07 14:03:24 +0100

    update vals retrieve

--- Utils.py
+++ Utils.py
@@ -8,12 +8,15 @@ def getRolesValues(conf):
     # convert roles to list of values only
     vals = []
     if hasattr(conf,'_roles'):
-        sroles = str(conf._roles).replace('false','False')
+        sroles = str(conf._roles).replace('false','False').replace('true','True')
         if sroles != '[]':
             lroles = eval(sroles)
             vals = []
             for r in lroles:
                 child = r['child']
                 for c in child:
-                    vals.append(c['value'])
+                    if hasattr(c,'familyName'):
+                        vals.append(c['familyName'])
+                    if hasattr(c,'firstName'):
+                        vals.append(c['firstName'])
     return ','.join(vals)
