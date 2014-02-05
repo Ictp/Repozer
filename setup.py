220ec19c1c73be9013601e363164de08841d94a5
ictp <pieretti@ictp.it>
2014-02-05 11:58:25 +0100

    added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+if __name__ == '__main__':
+    setup(
+        name='repozer',
+        version="0.9.1",
+        description="Indico search plugin",
+        author="Giorgio Pieretti",
+        packages=find_packages(),
+        include_package_data=True,
+        install_requires=["repoze.catalog",],
+        package_dir={'repozer': 'repozer'},
+        entry_points="""
+            [indico.ext]
+            search.repozer = indico.ext.search.repozer
+        """
+    )
