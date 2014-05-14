5e6a8563e390f69db3d5af7ea5a3ee3085ea1ca8
Ictp <pieretti@ictp.it>
2014-05-14 10:01:50 +0200

    Make use of separated catalogs

--- manage/createRepozeCatalog.py
+++ manage/createRepozeCatalog.py
@@ -40,8 +40,9 @@ indexMaterial = False
 db.DBMgr.getInstance().startRequest()
 rc = RepozeCatalog()
 
+
 def buildCatalog():
-    rc.init_catalog() # Forces index rebuild
+    #rc.init_catalog() # Forces index rebuild
     cm = CategoryManager()
     ch = ConferenceHolder()
     totnum = len(ch.getValuesToList())
@@ -56,34 +57,6 @@ def buildCatalog():
         if curnum >= startFrom:
             #if c and c.getId() == 'a12226':    
             print curnum,".......confid=",c.getId()
-            
-            
-            # CUSTOM CASES FOR ICTP
-#             if c.getRoles().find('\r\n') != -1:
-#                 c.setRoles(c.getRoles().replace('\r\n',''))
-#                 transaction.commit()
-#             if c.getId() == 'a0344':
-#                 c.setRoles('[{"id":2,"value":"Director(s)","editable":False,"child":[{"id":0,"familyName":"A. Simis"}, {"id":1,"familyName":"N.V. Trung and G. Valla"}]}, {"id":5,"value":"Director(s) & organizer(s)","editable":False,"child":[{"id":0,"familyName":"Scientific Committee: C. Huneke"}, {"id":1,"familyName":"A. Simis"}, {"id":2,"familyName":"B. Sturmfels"}, {"id":3,"familyName":"N.V. Trung"}, {"id":4,"familyName":"G. Valla and J. Verma"}]}, {"id":4,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"C. Huneke"}, {"id":1,"familyName":"A. Simis"}, {"id":2,"familyName":"N.V. Trung"}, {"id":3,"familyName":"G. Valla and J. Verma"}]}, {"id":1,"value":"Local organizer(s)","editable":False,"child":[{"id":0,"familyName":"Ramadas T. Ramakrishnan"}]}, {"id":1,"value":"Laboratories","editable":False,"child":[{"id":0,"familyName":"no"}]}, {"id":1,"value":"Secretary","editable":False,"child":[{"id":0,"familyName":"A. Bergamo"}]}, {"id":5,"value":"Cosponsor(s)","editable":False,"child":[{"id":0,"familyName":"Research Project -Commutative and Computer Algebra-"}, {"id":1,"familyName":"MIUR"}, {"id":2,"familyName":"and Department of Mathematics"}, {"id":3,"familyName":"University of Genoa - Italy"}]}]')
-#                 transaction.commit()
-#             if c.getId() == 'a0432':
-#                 c.setRoles('[{"id":2,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"Liceo Ginnasio Statale Francesco Petrarca"}, {"id":1,"familyName":"Trieste; contact: Prof. Marina Mai"}]}]')
-#                 transaction.commit()
-#             if c.getId() == 'a07198':
-#                 c.setRoles('[{"id":4,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"Prof. G.F. Panza (ICTP - ESP-SAND)"}, {"id":1,"familyName":"Prof. F.M. Mazzolani (University of Naples Federico II)"}, {"id":2,"familyName":"Ing. M. Indirli (ENEA-Bologna)."}]}, {"id":1,"value":"Secretary","editable":False,"child":[{"id":0,"familyName":"G. De Meo"}]}]')
-#                 transaction.commit()    
-#             if c.getId() == 'a08192':
-#                 c.setRoles('[{"id":7,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"Directors: S. Cozzini"}, {"id":1,"familyName":"P. Giannozzi"}, {"id":2,"familyName":"E. Menendez-Proupin"}, {"id":3,"familyName":"W. Orellana"}, {"id":4,"familyName":"S. ScandoloCo-Organizing Institutions: Universidad Andrés Bello (UNAB)"}, {"id":5,"familyName":"Santiago"}, {"id":6,"familyName":"Chile;Project Anillo ACT/24/2006 - Universidad de Chile -Computer Simulation Lab in Nanobio Systems- INFM - DEMOCRITOS  National Simulation Center"}]}, {"id":1,"value":"Secretary","editable":False,"child":[{"id":0,"familyName":"M. Poropat"}]}, {"id":3,"value":"Collaborations","editable":False,"child":[{"id":0,"familyName":"Universidad Andrés Bello (UNAB)"}, {"id":1,"familyName":"Santiago"}, {"id":2,"familyName":"Chile and Project Anillo ACT/24/2006 - Universidad de Chile -Computer Simulation Lab in Nanobio Systems-"}]}, {"id":2,"value":"Cosponsor(s)","editable":False,"child":[{"id":0,"familyName":"Comisión Nacional de Investigación Científica y Tecnológica (CONICYT)"}, {"id":1,"familyName":"Programa Bicentenario de Ciencia y Tecnología"}]}]')
-#                 transaction.commit()     
-#             if c.getId() == 'a09174':
-#                 c.setRoles('[{"id":4,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"Directors:  A. Belehaki"}, {"id":1,"familyName":"M. Messerotti"}, {"id":2,"familyName":"G.  Lapenta"}, {"id":3,"familyName":"S. Radicella"}]}, {"id":1,"value":"Laboratories","editable":False,"child":[{"id":0,"familyName":"AGH Infolab (afternoons)"}]}, {"id":1,"value":"Secretary","editable":False,"child":[{"id":0,"familyName":"S. Radosic"}]}, {"id":5,"value":"Cosponsor(s)","editable":False,"child":[{"id":0,"familyName":"EC COST Action ES0803 -Developing Products and Services for Space Weather in Europe-"}, {"id":1,"familyName":"EC FP7 Project SOTERIA -SOLar-TERrestrial Investigations and Archives-"}, {"id":2,"familyName":"National Institute for Astrophysics (INAF) European Space Agency (ESA)"}]}]')
-#                 transaction.commit()     
-#             if c.getId() == 'a09223':
-#                 c.setRoles('[{"id":5,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"M. Bianchi (University of Rome -Tor Vergata-)"}, {"id":1,"familyName":"S. Ferrara (CERN & INFN)"}, {"id":2,"familyName":"E. Kiritsis (University of Crete)"}, {"id":3,"familyName":"K. Narain (ICTP)"}, {"id":4,"familyName":"S. Randjbar-Daemi (ICTP) and A. Sen (HRI)"}]}, {"id":1,"value":"Secretary","editable":False,"child":[{"id":0,"familyName":"R. Sain"}]}, {"id":1,"value":"Cosponsor(s)","editable":False,"child":[{"id":0,"familyName":"the Asia Pacific Center for Theoretical Physics (APCTP) & the Italian Institute for Nuclear Physics (INFN)"}]}]')
-#                 transaction.commit()     
-#             if c.getId() == 'a0924':
-#                 c.setRoles('[{"id":2,"value":"Organizer(s)","editable":False,"child":[{"id":0,"familyName":"Liceo Ginnasio Statale -F. Petrarca- e Liceo -G. Galilei-"}, {"id":1,"familyName":"Trieste; contacts: Prof. Philip Tarsia and Ms. Renata Grill (Liceo Galilei)."}]}, {"id":1,"value":"Laboratories","editable":False,"child":[{"id":0,"familyName":"LB LAB"}]}]')
-#                 transaction.commit()     
-
                 
             rc.index(c, indexMaterial) 
             transaction.commit()
