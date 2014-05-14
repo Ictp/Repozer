375560325340bafb8de9941d16fc4d7a2009c380
ictp <pieretti@ictp.it>
2014-05-14 17:01:54 +0200

    all files moved under indico/ext/search/repozer

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from setuptools import setup, find_packages
+from setuptools import setup 
+#from setuptools import find_packages
+
 if __name__ == '__main__':
     setup(
         name='repozer',
-        version="0.9.3",
-        description="Indico search plugin",
+        version="0.9.4",
+        description=("Repozer search plugin for Indico"), 
         author="Giorgio Pieretti",
-        packages=find_packages(),
-        include_package_data=True,
-        #install_requires=["repoze.catalog",],
-        package_dir={'repozer': 'repozer'},
-        entry_points="""
-            [indico.ext]
-            search.repozer = indico.ext.search.repozer
-        """
+        author_email = "pieretti@ictp.it", 
+        #packages=find_packages(),
+        packages = ['indico', 'indico.ext', 'indico.ext.search',
+                'indico.ext.search.repozer',
+                'indico.ext.search.repozer.tpls',
+                'indico.ext.search.repozer.tpls.js',
+                'indico.ext.search.repozer.htdocs',
+                'indico.ext.search.repozer.htdocs.images'],
+        namespace_packages=['indico.ext.search'],
+        package_data={'indico.ext.search.repozer.tpls': ['*.tpl'],
+                    'indico.ext.search.repozer.tpls.js': ['*.js'],
+                  'indico.ext.search.repozer.htdocs.images': ['*.png']},
+        install_requires = ['indico'],
+        entry_points = {'indico.ext': ['search.repozer = indico.ext.search.repozer']}
     )
